3a93794fc258e4d74399b5f1cabebd1fb5489c92
jangsuhae <834425746@qq.com>
2019-04-10 10:37:31 +0800

    set encoding

--- src/com/qianfeng/servlet/HelloServlet.java
+++ src/com/qianfeng/servlet/HelloServlet.java
@@ -11,5 +11,6 @@ public class HelloServlet extends javax.servlet.http.HttpServlet {
     protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
         System.out.println("haha");
         response.getWriter().write("hello world");
+        response.setContentType("text/html;charset=utf-8");
     }
 }
