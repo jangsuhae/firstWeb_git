ccbd32a39f29d7ddba84faebec98c24674cb4b6c
jangsuhae <834425746@qq.com>
2019-04-10 09:59:07 +0800

    Initial commit

---
+++ src/com/qianfeng/servlet/HelloServlet.java
@@ -0,0 +1,15 @@
+package com.qianfeng.servlet;
+
+import java.io.IOException;
+
+@javax.servlet.annotation.WebServlet(name = "HelloServlet")
+public class HelloServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        System.out.println("haha");
+        response.getWriter().write("hello world");
+    }
+}
